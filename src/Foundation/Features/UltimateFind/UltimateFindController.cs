180273c85558516903effb4a7c811105f8a7aece
Karol Berezicki <karolberezicki@gmail.com>
2026-02-25 21:49:31 +0100

    added controller for testing

---
+++ src/Foundation/Features/UltimateFind/UltimateFindController.cs
@@ -0,0 +1,12 @@
+﻿namespace Foundation.Features.UltimateFind;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UltimateFindController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Ok");
+    }
+}
